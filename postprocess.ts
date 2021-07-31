c1017a59b3e259c50239a7d5fbf0cdf6c5130604
Timothée Peignier <timothee.peignier@icloud.com>
2021-07-30 20:51:50 -0700

    Tab tab

--- postprocess.ts
+++ postprocess.ts
@@ -2,6 +2,6 @@ import { readCSV, writeCSV } from 'https://deno.land/x/flat@0.0.11/mod.ts'
 
 const filename = Deno.args[0]
 const tsv = await readCSV(filename, {
-  separator: ' ',
+  separator: '\t',
 }) 
 console.log(tsv)
