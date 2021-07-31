d5bab42f11086344cf25b8657ca832e6e29a0eb8
Timothée Peignier <timothee.peignier@icloud.com>
2021-07-30 20:33:09 -0700

    Boop beep

--- postprocess.js
+++ postprocess.js
@@ -1,5 +1,5 @@
 import { readCSV, writeCSV } from 'https://deno.land/x/flat@0.0.11/mod.ts'
 
 const filename = Deno.args[0]
-const tsv = await readCSV(filename)
+const tsv = await readCSV(filename, {separator: ' '}) 
 console.log(tsv)
