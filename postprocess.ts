f1fa18db1ca58c32fbeaf1569c2230b97c071c16
Timothée Peignier <timothee.peignier@icloud.com>
2021-07-30 20:45:59 -0700

    Bug bug

--- postprocess.ts
+++ postprocess.ts
@@ -1,5 +1,7 @@
 import { readCSV, writeCSV } from 'https://deno.land/x/flat@0.0.11/mod.ts'
 
 const filename = Deno.args[0]
-const tsv = await readCSV(filename, {separator: ' '}) 
+const tsv = await readCSV(filename, {
+  separator: ' ',
+}) 
 console.log(tsv)
