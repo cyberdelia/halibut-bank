8ef0de285d8b32cd984c20776e5f1ef48709af30
Timothée Peignier <timothee.peignier@icloud.com>
2021-07-31 19:59:49 -0700

    Wave-y

--- postprocess.ts
+++ postprocess.ts
@@ -1,9 +1,21 @@
-import { readCSV, writeCSV } from 'https://deno.land/x/flat@0.0.11/mod.ts'
+import { readCSV, writeCSV } from "https://deno.land/x/flat@0.0.11/mod.ts";
 
-const filename = Deno.args[0]
+const filename = Deno.args[0];
+
 const tsv = await readCSV(filename, {
-  separator: ' ',
-  trimLeadingSpace: true
-}) 
-tsv.shift()
-writeCSV('buoy.csv', tsv)
+  separator: " ",
+  trimLeadingSpace: true,
+});
+tsv.shift();
+const waves = tsv.filter(function (row) {
+  return row["WVHT"] != "MM";
+});
+
+writeCSV("buoy.csv", waves);
+
+const swell = waves.filter(function (row) {
+  return (row["WDIR"] as number) >= 225 && (row["WVHT"] as number) >= 0.8;
+});
+if (swell.length > 0) {
+  console.log(swell);
+}
