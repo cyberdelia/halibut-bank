058489eb8cef7c75ad6e3cda86c3b862bcb66f37
Timothée Peignier <timothee.peignier@icloud.com>
2021-08-01 17:58:32 -0700

    Parse-y

--- postprocess.ts
+++ postprocess.ts
@@ -1,53 +1,30 @@
 import { readCSV, writeCSV } from "https://deno.land/x/flat@0.0.11/mod.ts";
+
 const filename = Deno.args[0];
-
-const header = [
-  "year",
-  "month",
-  "day",
-  "hour",
-  "minutes",
-  "wind direction",
-  "wind speed",
-  "gust speed",
-  "wave height",
-  "dominant wave period",
-  "average wave period",
-  "wave direction",
-  "pressure",
-  "air temperature",
-  "sea temperature",
-  "dewpoint",
-  "visibility",
-  "pressure tendency",
-  "tide",
-];
-
-const columns = [
-  "average wave period",
-  "wave direction",
-  "pressure",
-  "air temperature",
-  "sea temperature",
-  "dewpoint",
-  "visibility",
-  "pressure tendency",
-  "tide",
-];
 
 const tsv = await readCSV(filename, {
   separator: " ",
+  skipFirstRow: true,
   trimLeadingSpace: true,
-  columns: header,
+  parse: (row: any) => {
+    return {
+      date: `${row["#YY"]}-${row["MM"]}-${row["DD"]}T${row["hh"]}:${
+        row["mm"]
+      }:00Z`,
+      "wind direction": row["WDIR"],
+      "wind speed": row["WSPD"],
+      "gust speed": row["GST"],
+      "wave height": row["WVHT"],
+      "dominant wave period": row["DPD"],
+    };
+  },
 });
 
 tsv.shift();
 
-const waves = tsv.filter((row) => {
-  return row["wave height"] != "MM";
-}).map((row: any) => {
-  columns.forEach((column: string) => delete row[column]);
-  return row;
-});
-
-writeCSV("buoy.csv", waves);
+writeCSV(
+  "buoy.csv",
+  tsv.filter((row) => {
+    return row["wave height"] != "MM";
+  }),
+);
